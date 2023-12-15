9b0fe52c419094f606d3245dc73e806ce2799f4f
Ben Swift <benswift36@gmail.com>
2023-12-15 16:25:26 +0000

    started implementing firebase

---
+++ gym_app/lib/auth.dart
@@ -0,0 +1,36 @@
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> CreateUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      // Handle the error in a way that makes sense for your application.
+      print("Error signing out: $e");
+    }
+  }
+}
--- gym_app/lib/main.dart
+++ gym_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gym_app/social.dart';
 import 'package:gym_app/workouts.dart';
